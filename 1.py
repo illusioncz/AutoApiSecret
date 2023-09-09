68a7c6dc93abf4782421bbf097f68c534bf855c3
illusioncz <54033877+illusioncz@users.noreply.github.com>
2023-09-09 18:01:21 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+
 import requests as req
 import json,sys,time
 #先注册azure应用,确保应用有以下权限:
@@ -6,11 +6,6 @@ import json,sys,time
 #user:	User.Read.All、User.ReadWrite.All、Directory.Read.All、Directory.ReadWrite.All
 #mail:  Mail.Read、Mail.ReadWrite、MailboxSettings.Read、MailboxSettings.ReadWrite
 #注册后一定要再点代表xxx授予管理员同意,否则outlook api无法调用
-
-
-
-
-
 
 path=sys.path[0]+r'/1.txt'
 num1 = 0
